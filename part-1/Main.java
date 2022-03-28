cf9dd4cb360f196019103ab1c8006d8bb2f14c65
marcochiavaroliunibo <marco.chiavaroli@studio.unibo.it>
2022-03-28 14:46:07 +0200

    test with N body

--- part-1/Main.java
+++ part-1/Main.java
@@ -4,35 +4,26 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args){
-        Body b1 = new Body(1, new P2d(1,2), new V2d(1,2), 10);
-        Body b2 = new Body(2, new P2d(3,4), new V2d(3,4), 11);
-        Body b3 = new Body(3, new P2d(5,6), new V2d(5,6), 9);
-        Body b4 = new Body(4, new P2d(7,8), new V2d(7,8), 12);
 
-        List<Body> list = new ArrayList<Body>();
-        list.add(b1);
-        list.add(b2);
-        list.add(b3);
-        list.add(b4);
-
+        List<Body> list = new ArrayList<>();
+        List<BodyThread> btList = new ArrayList<>();
         Boundary boundary = new Boundary(5,5,5,5);
+        final int NUMBER_BODY = 500;
 
+        // Aggiungo N Body
+        for (int i=1,k=1;i<=NUMBER_BODY;i++,k+=2) {
+            Body body = new Body(i, new P2d(k, k+1), new V2d(k, k+1), k*2);
+            list.add(body);
+        }
         Monitor monitor = new Monitor(boundary,list);
 
-        BodyThread bt1 = new BodyThread(monitor,b1);
-        BodyThread bt2 = new BodyThread(monitor,b2);
-        BodyThread bt3 = new BodyThread(monitor,b3);
-        BodyThread bt4 = new BodyThread(monitor,b4);
+        // Aggiungo N BodyThread
+        for (int i=0;i<NUMBER_BODY;i++)
+            btList.add(new BodyThread(monitor, list.get(i)));
 
-        List<BodyThread> btList = new ArrayList<>();
-        btList.add(bt1);
-        btList.add(bt2);
-        btList.add(bt3);
-        btList.add(bt4);
-
-        for(int i = 0; i < 4; i++){
+        // Run
+        for (int i=0;i<NUMBER_BODY;i++)
             btList.get(i).start();
-        }
 
     }
 }
