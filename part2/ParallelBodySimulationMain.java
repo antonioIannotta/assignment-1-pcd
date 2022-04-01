ac29729286d234cb0dfd201a78c01c69fd664f21
antonioIannotta <74740099+antonioIannotta@users.noreply.github.com>
2022-04-01 14:51:02 +0200

    Add files via upload

--- part2/ParallelBodySimulationMain.java
+++ part2/ParallelBodySimulationMain.java
@@ -1,29 +1,75 @@
+import java.io.*;
+
 public class ParallelBodySimulationMain {
 
     public static void main(String[] args) {
 
         //PERFORMANCE NUMERO CORPI = 100 --- NUMERO ITERAZIONI = 1000
-
+        /*
         SimulationController controller = new SimulationController(100);
         SimulationView viewer = new SimulationView(controller, 600,600);
 
         long start = System.currentTimeMillis();
         controller.execute(1000, viewer);
         long stop = System.currentTimeMillis() - start;
-        System.out.println("Tempo di esecuzione con 100 corpi e 1000 iterazioni: " + stop );
 
+        try {
+            File simulationPerformanceTxt = new File("C:\\Users\\Antonio\\Desktop\\simulation_performance.txt");
+            if (simulationPerformanceTxt.createNewFile()) {
+                System.out.println("File creato: " + simulationPerformanceTxt.getName());
+            } else {
+                System.out.println("Il file esiste già.");
+            }
+        } catch (IOException e) {
+            System.out.println("Errore");
+            e.printStackTrace();
+        }
 
+         try {
+
+             FileWriter fw = new FileWriter("C:\\Users\\Antonio\\Desktop\\simulation_performance.txt", true);
+             BufferedWriter bw = new BufferedWriter(fw);
+             bw.write("Tempo di esecuzione con 100 corpi e 1000 iterazioni: " + stop + "ms");
+             bw.newLine();
+             bw.close();
+
+             System.out.println("Tempo di esecuzione con 100 corpi e 1000 iterazioni: " + stop + "ms");
+         } catch (Exception e) {
+             e.printStackTrace();
+         }
+            */
         // PERFORMANCE NUMERO CORPI = 100 --- NUMERO ITERAZIONI = 10000
 
-        /*
-        SimulationController controller = new SimulationController(100);
+        SimulationController controller = new SimulationController(1000);
         SimulationView viewer = new SimulationView(controller, 600,600);
 
         long start = System.currentTimeMillis();
-        controller.execute(10000, viewer);
+        controller.execute(50000, viewer);
         long stop = System.currentTimeMillis() - start;
-        System.out.println("Tempo di esecuzione con 100 corpi e 10000 iterazioni: " + stop );
-        */
+        try {
+            File simulationPerformanceTxt = new File("C:\\Users\\Antonio\\Desktop\\simulation_performance.txt");
+            if (simulationPerformanceTxt.createNewFile()) {
+                System.out.println("File creato: " + simulationPerformanceTxt.getName());
+            } else {
+                System.out.println("Il file esiste già.");
+            }
+        } catch (IOException e) {
+            System.out.println("Errore");
+            e.printStackTrace();
+        }
+
+         try {
+
+             FileWriter fw = new FileWriter("C:\\Users\\Antonio\\Desktop\\simulation_performance.txt", true);
+             BufferedWriter bw = new BufferedWriter(fw);
+             bw.write("Tempo di esecuzione con 1000 corpi e 50000 iterazioni: " + stop + "ms");
+             bw.newLine();
+             bw.close();
+
+             System.out.println("Tempo di esecuzione con 1000 corpi e 50000 iterazioni: " + stop + "ms");
+         } catch (Exception e) {
+             e.printStackTrace();
+         }
 
         //PERFORMANCE NUMERO CORPI = 100 --- NUMERO ITERAZIONI = 50000
         /*
